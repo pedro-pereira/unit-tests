f445beadc7ace6b91cac0dcadde556ecb20f430e
Pedro Pereira <pedro.pereira@techne.com.br>
2021-02-23 10:17:17 -0300

    Ajuste e criação de casos de teste
    Ajuste do caso "should throw team not found" e "should throw invalid ID". Criação do caso "verify first position".

--- src/teams/teams.test.js
+++ src/teams/teams.test.js
@@ -1,18 +1,28 @@
 const retrieveTeamMembers = require("./teams");
 
-describe("Team", function() {
+describe("Team", function () {
 
-    it("should return the teams members", function() {
+    it("should return the teams members", function () {
         expect(retrieveTeamMembers(5)).toEqual(["Caio Nunes", "Pedro Henrique", "Ulisses Lopes"]);
     });
 
-    it("should throw team not found", function() {
-        expect(() => retrieveTeamMembers(4)).toThrow("Team not found");
+    it("should throw team not found", function () {
+        // expect(() => retrieveTeamMembers(4)).toThrow("Team not found");
+        expect(retrieveTeamMembers(4)).toThrow("Team not found");
     });
 
-    it("should throw invalid ID", function() {
+    it("should throw invalid ID", function () {
         // expect(() => retrieveTeamMembers()).toThrow("Team not found");
-        expect(() => retrieveTeamMembers()).toThrow("Invalid ID");
+        // expect(() => retrieveTeamMembers()).toThrow("Invalid ID");
+        expect(retrieveTeamMembers()).toThrow("Invalid ID");
+    });
+
+    it("verify first position", function () {
+        expect(retrieveTeamMembers(5)[0]).toEqual("Caio Nunes");
+    });
+
+    it("verify first position", function () {
+        expect(retrieveTeamMembers(5)[0]).toEqual("Pedro Henrique");
     });
 
 })
